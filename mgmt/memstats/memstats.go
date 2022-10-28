e13e11e9f16fd1658378c0d1e991a4f3f70dc80c
Florent Clairambault <florent@clairambault.fr>
2022-10-28 14:35:06 +0200

    feature(mgmt): Adding cgstats to the memstats [release]
    #squash #merge #release

--- mgmt/memstats/memstats.go
+++ mgmt/memstats/memstats.go
@@ -5,14 +5,28 @@ import (
 	"runtime"
 
 	"github.com/gin-gonic/gin"
+	"github.com/habx/lib-go-utils/cgstats"
 )
+
+type memStats struct {
+	Runtime runtime.MemStats  `json:"runtime"`
+	CGroup  map[string]uint64 `json:"cgroup"`
+	Status  string            `json:"status,omitempty"`
+}
 
 // Handler returns the memory stats of the service
 func Handler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var m runtime.MemStats
+		var m memStats
 
-		runtime.ReadMemStats(&m)
+		runtime.ReadMemStats(&m.Runtime)
+
+		var err error
+		m.CGroup, err = cgstats.Get()
+
+		if err != nil {
+			m.Status += "cgstats:" + err.Error() + "\n"
+		}
 
 		c.JSON(http.StatusOK, m)
 	}
