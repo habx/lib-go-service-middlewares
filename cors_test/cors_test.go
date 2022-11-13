7f96430ab292aa25f7da39ed321a40288d5d0675
Florent Clairambault <florent@clairambault.fr>
2022-11-14 00:13:49 +0100

    feature(cors): Allowing force force header transmission
    #squash #merge #release

--- cors_test/cors_test.go
+++ cors_test/cors_test.go
@@ -1,6 +1,7 @@
 package cors_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -12,6 +13,21 @@ import (
 	"github.com/habx/lib-go-service-middlewares/cors"
 )
 
+func checkResponse(t *testing.T, req *http.Request, good bool) {
+	a := assert.New(t)
+
+	resp, err := http.DefaultClient.Do(req)
+	a.NoError(err)
+
+	defer resp.Body.Close()
+
+	if good {
+		a.Equal(http.StatusOK, resp.StatusCode)
+	} else {
+		a.Equal(http.StatusForbidden, resp.StatusCode)
+	}
+}
+
 func TestCors(t *testing.T) {
 	srv, eng := tgin.GetServerWithGin(t)
 
@@ -19,45 +35,47 @@ func TestCors(t *testing.T) {
 		c.String(http.StatusOK, "ok")
 	})
 
-	createReq := func(t *testing.T) *http.Request {
-		req, err := http.NewRequest(http.MethodPost, srv.URL("/test?name"+t.Name()), nil)
+	eng.POST("/test2", cors.Handler(cors.OptForceTransmission()), func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	createReq := func(t *testing.T, forceTransmission bool) *http.Request {
+		var path string
+		if forceTransmission {
+			path = "test2"
+		} else {
+			path = "test"
+		}
+
+		req, err := http.NewRequest(http.MethodPost, srv.URL(fmt.Sprintf("/%s?name%s", path, t.Name())), nil)
 		assert.NoError(t, err)
 
 		return req
 	}
 
-	check := func(t *testing.T, req *http.Request, good bool) {
-		a := assert.New(t)
-
-		resp, err := http.DefaultClient.Do(req)
-		a.NoError(err)
-
-		defer resp.Body.Close()
-
-		if good {
-			a.Equal(http.StatusOK, resp.StatusCode)
-		} else {
-			a.Equal(http.StatusForbidden, resp.StatusCode)
-		}
-	}
-
 	t.Run("good cors", func(t *testing.T) {
-		req := createReq(t)
+		req := createReq(t, false)
 		req.Header["Origin"] = []string{"https://www.habx.com"}
-		check(t, req, true)
+		checkResponse(t, req, true)
+	})
+
+	t.Run("good cors (force)", func(t *testing.T) {
+		req := createReq(t, true)
+		req.Header["Origin"] = []string{"https://www.habx.com"}
+		checkResponse(t, req, true)
 	})
 
 	t.Run("bad cors origin", func(t *testing.T) {
-		req := createReq(t)
+		req := createReq(t, false)
 		req.Header["Origin"] = []string{"https://www.google.com"}
-		check(t, req, false)
+		checkResponse(t, req, false)
 	})
 
 	t.Run("bad cors header", func(t *testing.T) {
 		t.Skip()
-		req := createReq(t)
+		req := createReq(t, false)
 		req.Header["Origin"] = []string{"https://www.habx.com"}
 		req.Header["Something"] = []string{"bad ?"}
-		check(t, req, false)
+		checkResponse(t, req, false)
 	})
 }
